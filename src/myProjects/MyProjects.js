4c5f8adfb590c0d5b35e63de1b48c455a028d8b9
wavewavewavewave <nikitashkrabov@gmail.com>
2021-12-19 15:42:22 +0300

    My Projects block

--- src/myProjects/MyProjects.js
+++ src/myProjects/MyProjects.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import s from './MyProjects.module.css';
+import styleContainer from "../common/styles/Container.module.css";
 
 
 function MyProjects(props) {
     return (
         <div className={s.projectsBlock}>
-            <div></div>
-            <h3>My Projects</h3>
+            <div className={` ${styleContainer.container}  ${s.projectsContainer}  `}>
+                <h3 className={s.title}>My Projects</h3>
+                <div className={s.projects}>
+
+                </div>
+            </div>
         </div>
     );
 }
